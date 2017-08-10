82bcd786c1d251a874e870f8c8c2356f8c84931e
Tom Hutchings <goerofmuns@gmail.com>
2017-08-10 01:03:35 +0100

    serve image from md5

--- server.rb
+++ server.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 require './booru.rb'
 
 boo = Booru.new
@@ -8,5 +9,11 @@ get '/' do
 end
 
 get '/image/:md5' do
-  erb :image, :locals => {:img => boo.request_md5(params['md5'])}
+  image = boo.request_md5(params['md5'])
+  erb :image, :locals => {:img => image}
 end
+
+get '/image/raw/:md5' do
+  send_file boo.request_md5(params['md5']).path
+end
+                                                             
